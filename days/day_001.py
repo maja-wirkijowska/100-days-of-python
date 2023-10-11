01cab8632de88504422e181144a6806984dd09be
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2023-10-10 20:16:43 -0700

    day one: band name generator

---
+++ days/day_001.py
@@ -0,0 +1,8 @@
+# 2023 October 10 - Day 1 - Band Name Generator
+print("Welcome to the Band Name Generator")
+print("What city did you grow up in? ")
+city = input()
+print("What is your pet's name? ")
+pet_name = input()
+band_name = city + " " + pet_name
+print(band_name)
