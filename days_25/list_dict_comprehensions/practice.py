a46ad1291683d9badb0e6eadbd1799efb013c040
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2023-11-08 19:07:36 -0700

    day 25 dictionary comprehension

--- days_25/list_dict_comprehensions/practice.py
+++ days_25/list_dict_comprehensions/practice.py
@@ -1,3 +1,4 @@
+print("LIST COMPREHENSION")
 numbers = [1, 1, 2, 3, 5, 8, 13, 21, 34, 55]
 squared_numbers = [n * n for n in numbers]
 print(f"1. {squared_numbers}")
@@ -12,3 +13,15 @@ with open("file2.txt") as file2:
     list2 = file2.readlines()
 result = [int(num) for num in list1 if num in list2]
 print(f"3. {result}")
+
+print("\nDICTIONARY COMPREHENSION")
+sentence = "What is the Airspeed Velocity of an Unladen Swallow?"
+sentence = sentence.split(" ")
+sentence_dict = {word: len(word) for word in sentence}
+print(f"1. {sentence_dict}")
+
+weather_c = {"Monday": 12, "Tuesday": 14, "Wednesday": 15, "Thursday": 14,
+             "Friday": 21, "Saturday": 22, "Sunday": 24}
+weather_f = {(day, temp * 9 / 5 + 32) for (day, temp) in weather_c.items()}
+print(f"2. {weather_f}")
+
