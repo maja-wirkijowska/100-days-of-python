8beecc66a36cefa87dc56342298c9e7492841473
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2023-11-08 18:38:05 -0700

    day 25 list comprehension added to state game in day 24

--- days_24/main.py
+++ days_24/main.py
@@ -19,10 +19,7 @@ while len(guessed_states) < 50:
     answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
                                     prompt="Enter the state's name: ").title()
     if answer_state == "Exit":
-        missing_states = []
-        for s in all_states:
-            if s not in guessed_states:
-                missing_states.append(s)
+        missing_states = [state for state in all_states if state not in guessed_states]
         new_data = pandas.DataFrame(missing_states).to_csv("states_to_learn.csv")
         break
     if answer_state in all_states:
