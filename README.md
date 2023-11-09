5d79c92966d58c3876e7907f7c46483d1aab73f9
maja-wirkijowska <97481772+maja-wirkijowska@users.noreply.github.com>
2023-11-08 17:53:40 -0700

    day 24

--- README.md
+++ README.md
@@ -24,3 +24,4 @@
 21. pong
 22. crossing turtle
 23. improved snake game
+24. name the 50 US states
---
+++ days_24/main.py
@@ -0,0 +1,36 @@
+import turtle
+import pandas
+
+
+def get_mouse_click_coordinate(x, y):
+    print(x, y)
+
+
+screen = turtle.Screen()
+screen.title("US STATES GAME")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+data = pandas.read_csv("50_states.csv")
+all_states = data.state.to_list()
+guessed_states = []
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 States Correct",
+                                    prompt="Enter the state's name: ").title()
+    if answer_state == "Exit":
+        missing_states = []
+        for s in all_states:
+            if s not in guessed_states:
+                missing_states.append(s)
+        new_data = pandas.DataFrame(missing_states).to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = data[data.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(state_data.state.item())
+
